85a6c2c56b5db06f7d0173bb89a49307e03e819b
David Leclerc <d.leclerc.pro@gmail.com>
2023-09-15 01:25:52 +0200

    Moved utils around.

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -3,3 +3,24 @@ export const round = (x: number, decimals: number) => {
 
     return Math.round(x * pow) / pow;
 }
+
+export const sum = (arr: number[]) => {
+    return arr.reduce((count, x) => count + x, 0);
+}
+
+export const getRange = (from: number, to?: number) => {
+    let size;
+
+    if (to === undefined) {
+        size = from;
+    } else {
+        if (from >= to) {
+            throw new Error(`'From' must be smaller than 'to'.`);
+        }
+
+        size = (to - from) + 1;
+    }
+
+    return [...Array(size).keys()]
+        .map(x => x + from);
+}
