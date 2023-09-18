f5c9886a7dbd7b8a659429207cde43d6a8c1e9d0
David Leclerc <d.leclerc.pro@gmail.com>
2023-09-18 10:43:42 +0200

    Wait 2 minutes max for spinner.

--- src/constants.ts
+++ src/constants.ts
@@ -15,6 +15,7 @@ export const MEDIUM_TIME = new TimeDuration(5, TimeUnit.Seconds);
 export const LONG_TIME = new TimeDuration(10, TimeUnit.Seconds);
 export const VERY_LONG_TIME = new TimeDuration(30, TimeUnit.Seconds);
 export const VERY_VERY_LONG_TIME = new TimeDuration(1, TimeUnit.Minutes);
+export const INFINITE_TIME = new TimeDuration(2, TimeUnit.Minutes); // More than 2 minutes in Internet terms is an eternity...
 
 export const ONE_HOUR = new TimeDuration(1, TimeUnit.Hours);
 export const FIVE_MINUTES = new TimeDuration(5, TimeUnit.Minutes);
