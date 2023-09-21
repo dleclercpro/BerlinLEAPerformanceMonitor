e9625d1b68b7778890a14dd150e51b393629abb9
David Leclerc <d.leclerc.pro@gmail.com>
2023-09-21 02:06:33 +0200

    Renaming variables. New logs.

--- src/config/git.ts
+++ src/config/git.ts
@@ -1,13 +1,13 @@
 import { getEnvironmentVariable } from '../utils/env';
 
-export const GIT_AUTHOR_NAME = getEnvironmentVariable('GIT_AUTHOR_NAME');
-export const GIT_AUTHOR_EMAIL = getEnvironmentVariable('GIT_AUTHOR_EMAIL');
-export const GIT_AUTHOR = { name: GIT_AUTHOR_NAME, email: GIT_AUTHOR_EMAIL };
+export const GIT_BOT_NAME = getEnvironmentVariable('GIT_BOT_NAME');
+export const GIT_BOT_EMAIL = getEnvironmentVariable('GIT_BOT_EMAIL');
+export const GIT_AUTHOR = { name: GIT_BOT_NAME, email: GIT_BOT_EMAIL };
 
-export const GITHUB_USER = getEnvironmentVariable('GITHUB_USER');
+export const GITHUB_BOT_USER = getEnvironmentVariable('GITHUB_BOT_USER');
 export const GITHUB_REPO_OWNER = getEnvironmentVariable('GITHUB_REPO_OWNER');
 export const GITHUB_REPO = getEnvironmentVariable('GITHUB_REPO');
-export const GITHUB_TOKEN = getEnvironmentVariable('GITHUB_TOKEN');
+export const GITHUB_BOT_TOKEN = getEnvironmentVariable('GITHUB_BOT_TOKEN');
 
-export const GIT_REMOTE_NAME = getEnvironmentVariable('GIT_REMOTE_NAME');
-export const GIT_REMOTE = { name: GIT_REMOTE_NAME, url: `https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/${GITHUB_REPO_OWNER}/${GITHUB_REPO}.git` };
+export const GIT_BOT_REMOTE = getEnvironmentVariable('GIT_BOT_REMOTE');
+export const GIT_REMOTE = { name: GIT_BOT_REMOTE, url: `https://${GITHUB_BOT_USER}:${GITHUB_BOT_TOKEN}@github.com/${GITHUB_REPO_OWNER}/${GITHUB_REPO}.git` };
